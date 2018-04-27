10464aaca87774b0c2a40d7314545b2047f104c9
Jenniffer Branco <JLo@MBPdeJenniffer.lan>
2018-04-27 19:37:07 +0100

    Class Logout added

--- Servlet/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ Servlet/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = "/*")
 public class FiltroDeAuditoria implements Filter {
@@ -23,13 +24,16 @@ public class FiltroDeAuditoria implements Filter {
 			throws IOException, ServletException {
 		
 		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
 		String uri = req.getRequestURI();
 		String nomeDoUsuario = "<deslogado>";
 		
-		Cookie cookie = getUsuario(req);
+		Cookie cookie = new Cookies(req.getCookies()).getUsuarioLogado();
 		
 		if(cookie != null) {
 			nomeDoUsuario = cookie.getValue();
+			cookie.setMaxAge(60 * 10);
+			resp.addCookie(cookie);
 		}
 		
 		System.out.println("Usuário " + nomeDoUsuario + " acessando: " + uri);	
@@ -39,23 +43,6 @@ public class FiltroDeAuditoria implements Filter {
 		
 	}
 
-	private Cookie getUsuario(HttpServletRequest req) {
-
-		Cookie[] cookies = req.getCookies();
-		
-		if (cookies == null) {
-			return null;
-		}
-		
-		for (Cookie cookie : cookies) {
-			if (cookie.getName()	.equals("usuario-logado")) {
-				return cookie;
-			}
-		}
-		
-		return null;
-		
-	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {}
